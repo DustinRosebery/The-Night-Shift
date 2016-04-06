90050410dba405a394f687a0415961944bce2ec5
tcrosenk <tcrosenk@asu.edu>
2016-04-05 22:00:40 -0700

    Dice Class implementation
    The dice class has a single static method "roll()" that will return the sum of three dice added together. Simply assign Dice.roll() to an int to use the value.

---
+++ src/Dice.java
@@ -0,0 +1,24 @@
+/** This is a dice class with a single static method that will return the sum of 
+ * selected amount of dice to be rolled.
+ * 
+ * @author Trevor Rosenkilde
+ * @version April 5, 2016
+ */
+public class Dice {
+	
+	private final static int NUM_OF_DICE = 3; //number of dice to be rolled
+	
+	/** This method will return the sum of all the dice rolled.
+	 * 
+	 * @return (int) sum of the all of the rolled dice
+	 */
+	public static int roll() { 
+		int sumOfDiceRoll = 0; //initialize the variable
+		for (int index = 0; index < NUM_OF_DICE; index++) { //will run for how many dice there are
+			int diceRoll = (int)(Math.random() * 6 + 1); //utilizes Math.random() to get the random effect of a dice roll
+			sumOfDiceRoll = sumOfDiceRoll + diceRoll; //add to the sum
+			//System.out.println("Roll #" + index + ": " + diceRoll); //testing purposes to see what was rolled
+		}
+		return sumOfDiceRoll; //return the overall sum of the rolled dice
+	}
+}
