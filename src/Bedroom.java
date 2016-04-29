0be174679f27715c7a5a41a409bcf42bf98c97da
kklan <klanjewa@asu.edu>
2016-04-29 13:39:59 -0700

    created bedroom

--- src/Bedroom.java
+++ src/Bedroom.java
@@ -1,5 +1,5 @@
 /**
- * Created by dustinrosebery on 4/13/16.
+ * @author Kunal Lanjewar
  */
 public class Bedroom extends Rooms {
 
@@ -13,5 +13,69 @@ public class Bedroom extends Rooms {
         description = "you are in the bedroom";
 
         exits = "Living room - kitchen";
+    }
+
+    @Override
+    public void entry(Character character) {
+        Interpreter interpreter = new Interpreter(input -> {
+            boolean handled = false;
+            String[] args = input.toString().split(" ");
+
+            if (handled = args[0].equalsIgnoreCase("take")) {
+                boolean validItem = false;
+                int itemIndex = -1;
+                String skill = "";
+                String name = "";
+
+                if (args.length >= 2) {
+                    if (args[1].equalsIgnoreCase("roomba")) {
+                        validItem = true;
+                        itemIndex = 25;
+                        skill = "reflex";
+                        name = "Roomba Vacuum Cleaner";
+                    } else if (args[1].equalsIgnoreCase("samsungtv")) {
+                        validItem = true;
+                        itemIndex = 26;
+                        skill = "intelligence";
+                        name = "Samsung Curve TV ";
+                    } else if (args[1].equalsIgnoreCase("playstation")) {
+                        validItem = true;
+                        itemIndex = 27;
+                        skill = "dexterity";
+                        name = "Playstation4";
+                    } else if (args[1].equalsIgnoreCase("painting")) {
+                        validItem = true;
+                        itemIndex = 28;
+                        skill = "perception";
+                        name = "Rare Painting";
+                    } else if (args[1].equalsIgnoreCase("fitnessequipment")) {
+                        validItem = true;
+                        itemIndex = 29;
+                        skill = "strength";
+                        name = "Fitness Equipment";
+                    } else {
+                        validItem = false;
+                        Game.getController().write("There are many thing to steal. Explore the bedroom!");
+                    }
+                    if (validItem) {
+                        if (Game.getCurrentCharacter().inventory().getList().contains(Items.itemList.get(itemIndex))) {
+                            if (Game.getCurrentCharacter().skillCheck(skill)) {
+                                Game.getCurrentCharacter().addItem(Items.itemList.get(itemIndex));
+                                Game.getController().write("Nice job! You took " + name + " successfully");
+                            } else {
+                                Game.getController().handleFailure();
+                            }
+                        }
+                    }
+                } else {
+                    Game.getController().write("So, What do you want to steal?");
+                }
+
+            }
+            return handled;
+        });
+
+        setInterpreter(interpreter);
+
     }
 }
